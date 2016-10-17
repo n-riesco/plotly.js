c667e78affb911850889088dd8a47b678ed24c48
Nicolas Riesco <enquiries@nicolasriesco.net>
2016-10-17 14:21:45 +0100

    test: layout.barbase

--- test/jasmine/tests/bar_test.js
+++ test/jasmine/tests/bar_test.js
@@ -61,46 +61,134 @@ describe('bar supplyDefaults', function() {
     });
 });
 
-describe('heatmap calc / setPositions', function() {
+describe('bar supplyLayoutDefaults', function() {
+    'use strict';
+
+    var gd;
+
+    it('should set barbase to 0', function() {
+        gd = {
+            data: [{
+                type: 'bar',
+                x: [],
+                y: [],
+            }],
+            layout: {}
+        };
+
+        Plots.supplyDefaults(gd);
+
+        expect(gd._fullLayout.barbase).toBe(0);
+    });
+});
+
+describe('barbase', function() {
     'use strict';
 
     beforeAll(function() {
         jasmine.addMatchers(customMatchers);
     });
 
-    function _calc(dataOpts, layout) {
-        var baseData = { type: 'bar' };
+    function assertPointField(calcData, prop, expectation) {
+        var values = [];
 
-        var data = dataOpts.map(function(traceOpts) {
-            return Lib.extendFlat({}, baseData, traceOpts);
+        calcData.forEach(function(calcTrace) {
+            var vals = calcTrace.map(function(pt) {
+                return Lib.nestedProperty(pt, prop).get();
+            });
+
+            values.push(vals);
         });
 
-        var gd = {
-            data: data,
-            layout: layout,
-            calcdata: []
-        };
-
-        Plots.supplyDefaults(gd);
-
-        gd._fullData.forEach(function(fullTrace) {
-            var cd = Bar.calc(gd, fullTrace);
-
-            cd[0].t = {};
-            cd[0].trace = fullTrace;
-
-            gd.calcdata.push(cd);
-        });
-
-        var plotinfo = {
-            xaxis: gd._fullLayout.xaxis,
-            yaxis: gd._fullLayout.yaxis
-        };
-
-        Bar.setPositions(gd, plotinfo);
-
-        return gd.calcdata;
+        expect(values).toBeCloseTo2DArray(
+            expectation, undefined, '- field ' + prop);
     }
+
+    it('should be honored in overlay mode', function() {
+        var data = [{
+                y: [10, 20, 30]
+            }, {
+                y: [-10, 20, -30]
+            }, {
+                y: [null, null, -30]
+            }],
+            layout = {
+                barbase: 10,
+                barmode: 'overlay'
+            },
+            cd = getCalcdata(data, layout);
+
+        // Note:
+        // The base of null bars is set to zero and the size is left undefined
+        assertPointField(cd, 'b', [[10, 10, 10], [10, 10, 10], [0, 0, 10]]);
+        assertPointField(cd, 's',
+            [[0, 10, 20], [-20, 10, -40], [undefined, undefined, -40]]);
+        assertPointField(cd, 'p', [[0, 1, 2], [0, 1, 2], [0, 1, 2]]);
+        assertPointField(cd, 'x', [[0, 1, 2], [0, 1, 2], [0, 1, 2]]);
+        assertPointField(cd, 'y',
+            [[10, 20, 30], [-10, 20, -30], [undefined, undefined, -30]]);
+    });
+
+    it('should be honored in group mode', function() {
+        var data = [{
+                y: [10, 20, 30]
+            }, {
+                y: [-10, 20, -30]
+            }, {
+                y: [null, null, -30]
+            }],
+            layout = {
+                barbase: 10,
+                barmode: 'group'
+            },
+            cd = getCalcdata(data, layout);
+
+        // Note:
+        // The base of null bars is set to zero and the size is left undefined
+        assertPointField(cd, 'b', [[10, 10, 10], [10, 10, 10], [0, 0, 10]]);
+        assertPointField(cd, 's',
+            [[0, 10, 20], [-20, 10, -40], [undefined, undefined, -40]]);
+        assertPointField(cd, 'p', [[0, 1, 2], [0, 1, 2], [0, 1, 2]]);
+        assertPointField(cd, 'x',
+            [[-0.27, 0.73, 1.73], [0, 1, 2], [0.27, 1.27, 2.27]]);
+        assertPointField(cd, 'y',
+            [[10, 20, 30], [-10, 20, -30], [undefined, undefined, -30]]);
+    });
+
+    it('should be honored in group mode when barnorm is set', function() {
+        var data = [{
+                y: [30, 70, 20]
+            }, {
+                y: [70, 30, 30]
+            }, {
+                y: [null, null, 50]
+            }],
+            layout = {
+                barbase: 1,
+                barmode: 'group',
+                barnorm: 'fraction'
+            },
+            cd = getCalcdata(data, layout);
+
+        // Note:
+        // The base of null bars is set to zero and the size is left undefined
+        assertPointField(cd, 'b', [[1, 1, 1], [1, 1, 1], [0, 0, 1]]);
+        assertPointField(cd, 's',
+            [[-0.7, -0.3, -0.8], [-0.3, -0.7, -0.7], [undefined, undefined, -0.5]]);
+        assertPointField(cd, 'p', [[0, 1, 2], [0, 1, 2], [0, 1, 2]]);
+        assertPointField(cd, 'x',
+            [[-0.27, 0.73, 1.73], [0, 1, 2], [0.27, 1.27, 2.27]]);
+        assertPointField(cd, 'y',
+            [[0.3, 0.7, 0.2], [0.7, 0.3, 0.3], [undefined, undefined, 0.5]]);
+    });
+});
+
+describe('heatmap calc / setPositions', function() {
+    'use strict';
+
+    beforeAll(function() {
+        jasmine.addMatchers(customMatchers);
+    });
 
     function assertPtField(calcData, prop, expectation) {
         var values = [];
@@ -125,7 +213,7 @@ describe('heatmap calc / setPositions', function() {
     }
 
     it('should fill in calc pt fields (stack case)', function() {
-        var out = _calc([{
+        var out = getCalcdata([{
             y: [2, 1, 2]
         }, {
             y: [3, 1, 2]
@@ -146,7 +234,7 @@ describe('heatmap calc / setPositions', function() {
     });
 
     it('should fill in calc pt fields (overlay case)', function() {
-        var out = _calc([{
+        var out = getCalcdata([{
             y: [2, 1, 2]
         }, {
             y: [3, 1, 2]
@@ -165,7 +253,7 @@ describe('heatmap calc / setPositions', function() {
     });
 
     it('should fill in calc pt fields (group case)', function() {
-        var out = _calc([{
+        var out = getCalcdata([{
             y: [2, 1, 2]
         }, {
             y: [3, 1, 2]
@@ -186,7 +274,7 @@ describe('heatmap calc / setPositions', function() {
     });
 
     it('should fill in calc pt fields (relative case)', function() {
-        var out = _calc([{
+        var out = getCalcdata([{
             y: [20, 14, -23]
         }, {
             y: [-12, -18, -29]
@@ -205,7 +293,7 @@ describe('heatmap calc / setPositions', function() {
     });
 
     it('should fill in calc pt fields (relative / percent case)', function() {
-        var out = _calc([{
+        var out = getCalcdata([{
             x: ['A', 'B', 'C', 'D'],
             y: [20, 14, 40, -60]
         }, {
@@ -226,3 +314,37 @@ describe('heatmap calc / setPositions', function() {
         assertTraceField(out, 't.bargroupwidth', [0.8, 0.8]);
     });
 });
+
+function getCalcdata(dataOpts, layout) {
+    var baseData = { type: 'bar' };
+
+    var data = dataOpts.map(function(traceOpts) {
+        return Lib.extendFlat({}, baseData, traceOpts);
+    });
+
+    var gd = {
+        data: data,
+        layout: layout,
+        calcdata: []
+    };
+
+    Plots.supplyDefaults(gd);
+
+    gd._fullData.forEach(function(fullTrace) {
+        var cd = Bar.calc(gd, fullTrace);
+
+        cd[0].t = {};
+        cd[0].trace = fullTrace;
+
+        gd.calcdata.push(cd);
+    });
+
+    var plotinfo = {
+        xaxis: gd._fullLayout.xaxis,
+        yaxis: gd._fullLayout.yaxis
+    };
+
+    Bar.setPositions(gd, plotinfo);
+
+    return gd.calcdata;
+}
